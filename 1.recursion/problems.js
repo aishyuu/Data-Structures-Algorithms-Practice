001841fe9c93ac2c719514017c66090bd6d5820a
aishyuu <ivanmatias1331@gmail.com>
2024-03-01 16:10:57 -0800

    Did more problems

--- 1.recursion/problems.js
+++ 1.recursion/problems.js
@@ -1,3 +1,4 @@
+// Sum all numbers
 function sumRange(n) {
   if (n == 1) {
     return 1;
@@ -7,6 +8,7 @@ function sumRange(n) {
 
 console.log(sumRange(3));
 
+// Power Function
 function power(base, exponent) {
   if (exponent === 0) {
     return 1;
@@ -16,6 +18,7 @@ function power(base, exponent) {
 
 console.log(power(2, 4));
 
+// Calculate factorial
 function factorial(n) {
   if (n === 1) {
     return 1;
@@ -25,12 +28,29 @@ function factorial(n) {
 
 console.log(factorial(5));
 
+// Product of an array
 function productOfArray(arr) {
   if (arr.length === 1) {
     return arr[0];
   }
-  let a = arr.shift()
+  let a = arr.shift();
   return a * productOfArray(arr);
 }
 
 console.log(productOfArray([1, 2, 3, 10]));
+
+// Check all values in an array
+function checkAllVals(arr, check) {
+  if (arr.length == 1) {
+    return check(arr[0]);
+  }
+  if (check(arr.pop())) {
+    return checkAllVals(arr, check);
+  }
+}
+
+console.log(
+  checkAllVals([1, 2, 9], function (num) {
+    return num < 7;
+  })
+);

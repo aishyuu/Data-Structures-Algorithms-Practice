813008999b02eab87f4ca93d807515fcc81abe62
aishyuu <ivanmatias1331@gmail.com>
2024-03-04 23:42:15 -0800

    Added linked list basic class constructors

--- 5.linked_lists/linked_lists.js
+++ 5.linked_lists/linked_lists.js
@@ -1,1 +1,15 @@
 // Linked List implementation here!
+class Node {
+  constructor() {
+    this.value = null;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.size = 0;
+    this.head = null;
+    this.tail = null;
+  }
+}

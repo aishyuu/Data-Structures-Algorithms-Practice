b601bd05206124404f2710a0cffabadc0468a4b7
aishyuu <ivanmatias1331@gmail.com>
2024-03-05 14:28:53 -0800

    Added 'append', 'prepend' and 'toString' methods in Linked List class

--- 5.linked_lists/linked_lists.js
+++ 5.linked_lists/linked_lists.js
@@ -12,4 +12,61 @@ class LinkedList {
     this.head = null;
     this.tail = null;
   }
+  append(value) {
+    const newNode = new Node();
+    newNode.value = value;
+    if (this.size == 0) {
+      this.head = newNode;
+      this.tail = newNode;
+      this.size += 1;
+      return;
+    }
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.size += 1;
+    return;
+  }
+
+  prepend(value) {
+    const newNode = new Node();
+    newNode.value = value;
+    if (this.size == 0) {
+      this.head = newNode;
+      this.tail = newNode;
+      this.size += 1;
+      return;
+    }
+    newNode.next = this.head;
+    this.head = newNode;
+    this.size += 1;
+  }
+
+  toString() {
+    if (this.size == 0) {
+      console.log("Nothing inside linked list (yet)");
+      return;
+    }
+    let currentNode = this.head;
+    let allValues = [];
+    while (true) {
+      allValues.push(currentNode.value);
+      if (currentNode.next == null) {
+        break;
+      }
+      currentNode = currentNode.next;
+    }
+    let resultString = "";
+    for(let i = 0; i < allValues.length; i++) {
+        resultString += `(${allValues[i]}) -> `
+    }
+    resultString += "(null)"
+    return resultString;
+  }
 }
+
+const list = new LinkedList();
+list.append(5);
+list.prepend(6);
+list.append(1);
+
+console.log(list.toString());

26de8fdc0b4182e3d0ad1145d5c51305a11b3ce3
aishyuu <ivanmatias1331@gmail.com>
2024-03-07 16:27:22 -0800

    Added constructor and hash methods to the Hash Table class

--- 6.hash_table/hashtable.js
+++ 6.hash_table/hashtable.js
@@ -6,7 +6,7 @@ class Node {
   }
 }
 
-export class LinkedList {
+class LinkedList {
   constructor() {
     this.size = 0;
     this.head = null;
@@ -169,3 +169,26 @@ export class LinkedList {
 }
 
 // HASH TABLE STARTS HERE
+class HashTable {
+  constructor(size) {
+    this.len = 0;
+    this.data = new Array(size);
+    for (let i = 0; i < this.data.length; i++) {
+      this.data[i] = new LinkedList();
+    }
+  }
+
+  hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+    }
+
+    return hashCode;
+  }
+}
+
+let test = new HashTable(12);
+console.log(test.data);

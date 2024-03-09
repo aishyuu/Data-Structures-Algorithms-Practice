b1c55a85d7370fd24b7b25071213ef4efe2c13fc
aishyuu <ivanmatias1331@gmail.com>
2024-03-08 23:53:10 -0800

    Completed buildTree method in the BinarySearchTree class

--- 7.binary_search_tree/bst.js
+++ 7.binary_search_tree/bst.js
@@ -1,13 +1,55 @@
 class Node {
-    constructor() {
-        this.value = null;
-        this.right = null;
-        this.left = null;
-    }
+  constructor(val) {
+    this.value = val;
+    this.right = null;
+    this.left = null;
+  }
 }
 
 class BinarySearchTree {
-    constructor() {
-        this.root = null;
+  constructor() {
+    this.root = null;
+  }
+
+  buildTree(arr) {
+    this.root = new Node(arr[0]);
+    let currentNode = this.root;
+
+    for (let i = 1; i < arr.length; i++) {
+      while (true) {
+        if (arr[i] >= currentNode.value) {
+          if (currentNode.right == null) {
+            currentNode.right = new Node(arr[i]);
+            break;
+          }
+          currentNode = currentNode.right;
+          continue;
+        }
+        if (currentNode.left == null) {
+          currentNode.left = new Node(arr[i]);
+          break;
+        }
+        currentNode = currentNode.left;
+      }
+      currentNode = this.root;
     }
+  }
 }
+
+let test = new BinarySearchTree();
+test.buildTree([10, 7, 5, 9, 13, 16, 19, 14]);
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  }
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+
+prettyPrint(test.root);

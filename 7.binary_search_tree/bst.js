c3fb5a1f37f7504bf8a48ec150997cd524034274
aishyuu <ivanmatias1331@gmail.com>
2024-03-08 21:07:54 -0800

    Created Node class and BinarySearchTree class with their constructors

---
+++ 7.binary_search_tree/bst.js
@@ -0,0 +1,13 @@
+class Node {
+    constructor() {
+        this.value = null;
+        this.right = null;
+        this.left = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+}

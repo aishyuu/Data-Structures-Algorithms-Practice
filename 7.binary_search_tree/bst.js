3cb92f5d19bcc2193293ca766ee181e3bf36d9af
aishyuu <ivanmatias1331@gmail.com>
2024-03-11 20:35:35 -0700

    Added traversals

--- 7.binary_search_tree/bst.js
+++ 7.binary_search_tree/bst.js
@@ -104,6 +104,125 @@ class BinarySearchTree {
       return root;
     }
   }
+
+  find(val) {
+    let currentNode = this.root;
+    while (true) {
+      if (currentNode == null) {
+        console.log(`Could not find ${val}`);
+        return;
+      }
+      if (val > currentNode.value) {
+        currentNode = currentNode.right;
+      } else if (val < currentNode.value) {
+        currentNode = currentNode.left;
+      } else if (val == currentNode.value) {
+        return currentNode;
+      }
+    }
+  }
+
+  levelOrder() {
+    if (this.root == null) return [];
+    const queue = [this.root];
+    const results = [];
+    while (queue.length) {
+      let level = [];
+      let size = queue.length;
+      for (let i = 0; i < size; i++) {
+        const node = queue.shift();
+        level.push(node.value);
+        if (node.left != null) {
+          queue.push(node.left);
+        }
+        if (node.right != null) {
+          queue.push(node.right);
+        }
+      }
+      results.push(level);
+    }
+    return results;
+  }
+
+  preOrder() {
+    // root, left, right
+    if (this.root == null) {
+      return [];
+    }
+    const stack = [this.root];
+    const results = [];
+    while (stack.length) {
+      const node = stack.pop();
+      if (node.right != null) {
+        stack.push(node.right);
+      }
+      if (node.left != null) {
+        stack.push(node.left);
+      }
+      results.push(node.value);
+    }
+    return results;
+  }
+
+  inOrder(node = this.root, result = []) {
+    if(this.root != null) {
+        return [];
+    }
+    if(node == null) {
+        return;
+    }
+    this.inOrder(node.left, result);
+    this.inOrder(node.right, result);
+    if(result) return result;
+  }
+
+  postOrder() {
+    if(this.root == null) {
+        return [];
+    }
+    const stack = [this.root];
+    const results = [];
+    while(stack.length) {
+        const node = stack.pop();
+        if(node.left != null) {
+            stack.push(node.left);
+        }
+        if(node.right != null) {
+            stack.push(node.right);
+        }
+        results.push(node.key);
+    }
+    return results.reverse();
+  }
+
+  height(node = this.root) {
+    if(node == null) {
+        return -1;
+    }
+    const leftHeight = this.height(node.left);
+    const rightHeight = this.height(node.right);
+    return Math.max(leftHeight, rightHeight) + 1;
+  }
+
+  depth(node, root = this.root, level = 0) {
+    if (node == null) {
+        return null;
+    }
+
+    if(root == null) {
+        return 0;
+    }
+
+    if(root.value == node.value) {
+        return level;
+    }
+
+    let count = this.depth(node, root.left, level + 1);
+    if(count != 0) {
+        return count;
+    }
+    return this.depth(node, root.right, level + 1);
+  }
 }
 
 let test = new BinarySearchTree();
@@ -124,3 +243,4 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
 test.insert(17);
 test.deleteNode(16);
 prettyPrint(test.root);
+console.log(test.levelOrder());

211b75d2e523cee233ef80db541ce8c4b896e206
aishyuu <ivanmatias1331@gmail.com>
2024-03-11 19:30:37 -0700

    Added 'deleteNode' method to BinarySearchTree class

--- 7.binary_search_tree/bst.js
+++ 7.binary_search_tree/bst.js
@@ -61,6 +61,49 @@ class BinarySearchTree {
       }
     }
   }
+
+  deleteNode(val, root = this.root) {
+    if (root == null) {
+      return root;
+    }
+
+    if (val > root.value) {
+      root.right = this.deleteNode(val, root.right);
+      return root;
+    } else if (val < root.value) {
+      root.left = this.deleteNode(val, root.left);
+      return root;
+    }
+
+    if (root.left == null) {
+      let temp = root.right;
+      root = null;
+      return temp;
+    } else if (root.right == null) {
+      let temp = root.left;
+      root = null;
+      return temp;
+    } else {
+      let succParent = root;
+
+      let succ = root.right;
+      while (succ.left != null) {
+        succParent = succ;
+        succ = succ.left;
+      }
+
+      if (succParent != root) {
+        succParent.left = succ.right;
+      } else {
+        succParent.right = succ.right;
+      }
+
+      root.value = succ.value;
+
+      succ = null;
+      return root;
+    }
+  }
 }
 
 let test = new BinarySearchTree();
@@ -79,4 +122,5 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
   }
 };
 test.insert(17);
+test.deleteNode(16);
 prettyPrint(test.root);
